92cdc5c62dcf31e2bdbe06d8852e3dc8fc75ae29
trion129 <parmsingh129@yahoo.com>
2016-05-07 18:38:30 +0530

    All good now

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ app.get("/",function(req,res){
 	var object = {};
 	object.ip = req.ip;
 	object.language = req.headers["accept-language"][0];
-	object.os = req.headers['user-agent'];
+	object.os = req.headers['user-agent'].match(/\((.*?)\)/gi)[0];
+	object.os = object.os.substring(1,object.os.length-1);
 	res.end(JSON.stringify(object));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(/*process.env.PORT || */8080);
